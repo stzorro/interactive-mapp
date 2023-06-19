340f853f0affc9d86d9f6fec19797b12f15bbf38
stzorro <137074105+stzorro@users.noreply.github.com>
2023-06-19 15:03:20 +0200

    Add files via upload

---
+++ interactive_map.js
@@ -0,0 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.3.0/raphael.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.3.0/js/jquery.mapael.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.3.0/js/maps/sweden_counties.min.js"></script>
